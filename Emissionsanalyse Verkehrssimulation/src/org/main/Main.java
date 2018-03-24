cdef3caabd5366190e16f5991cf6c9e763e12466
Marco Wiedmann <marco_wiedmann@web.de>
2018-03-24 10:02:18 +0100

    Synchronisation mit Schulrechner

--- Emissionsanalyse Verkehrssimulation/src/org/main/Main.java
+++ Emissionsanalyse Verkehrssimulation/src/org/main/Main.java
@@ -5,7 +5,7 @@ import org.Verkehr.PKW;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stu
+		// TODO Auto-generated method stub
 		for(int i = 0; i < 1000; i++) {
 			PKW auto = new PKW();
 			System.out.println(auto.getWirkungsgrad());
